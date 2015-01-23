366dd1ee781506e033c1ccc6c77f026b068bdc3e
FreeSirenety <revilo.tlob@gmail.com>
2015-01-23 19:40:19 +0100

    Updated gitignore and stfufdfaf

---
+++ Source/WhatNow/Public/WhatNowGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "WhatNowGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWhatNowGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AWhatNowGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
